856a33c64c1b797ddd5fb34c193806d04df68de4
Joshua Immordino <joshimmor@gmail.com>
2024-01-30 22:33:53 -0500

    diart proof

--- app/Components/MasterDeck.js
+++ app/Components/MasterDeck.js
@@ -14,8 +14,7 @@ export default function MasterDeck({blocks,setBlocks,keyToggle}) {
         })
         .then( n => n.json())
         .then(n => setYoutube(n.items))
-        .then(n => console.log(youtubeLinks))
-        .catch(err => console.error(err))
+        .catch(err => setYoutube([]))
     }
     let handleClick = (e) =>{
         let rect = e.target.getBoundingClientRect()
@@ -47,11 +46,10 @@ export default function MasterDeck({blocks,setBlocks,keyToggle}) {
         console.log(e.key)
     }
     useEffect(()=>{
-        SetPlaylist()
         if(keyToggle  && keyToggle <= blocks.length){
             handleVisiblity(blocks[keyToggle-1].visable,keyToggle-1)
         }
-    },[keyToggle,SetPlaylist,handleVisiblity,blocks])
+    },[keyToggle,handleVisiblity,blocks])
   return (
     <div className={`absolute top-0 left-0  rounded-md  w-[5vw] h-[100vh] flex flex-col justify-between`}
 
@@ -85,6 +83,9 @@ export default function MasterDeck({blocks,setBlocks,keyToggle}) {
                     
         </div>
         <div className='z-10  w-full flex flex-col justify-start'>
+             <button className="flex justify-center items-center mt-5 text-slate-900 font-bold border-1 border-slate-600 py-2  hover:bg-slate-900 hover:text-slate-200 "  >
+             <svg width="84px" height="84px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.5" d="M16 4.00195C18.175 4.01406 19.3529 4.11051 20.1213 4.87889C21 5.75757 21 7.17179 21 10.0002V16.0002C21 18.8286 21 20.2429 20.1213 21.1215C19.2426 22.0002 17.8284 22.0002 15 22.0002H9C6.17157 22.0002 4.75736 22.0002 3.87868 21.1215C3 20.2429 3 18.8286 3 16.0002V10.0002C3 7.17179 3 5.75757 3.87868 4.87889C4.64706 4.11051 5.82497 4.01406 8 4.00195" stroke="#1C274C" stroke-width="1.5"></path> <path d="M8 14H16" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M7 10.5H17" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M9 17.5H15" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M8 3.5C8 2.67157 8.67157 2 9.5 2H14.5C15.3284 2 16 2.67157 16 3.5V4.5C16 5.32843 15.3284 6 14.5 6H9.5C8.67157 6 8 5.32843 8 4.5V3.5Z" stroke="#1C274C" stroke-width="1.5"></path> </g></svg>
+            </button>
             <button className="flex justify-center items-center mt-5 text-slate-900 font-bold border-1 border-slate-600 py-2  hover:bg-slate-900 hover:text-slate-200 "  onClick={()=>index > 0 ?setIndex(index-1):setIndex(youtubeLinks.length-1)} >
             <svg width="85px" height="85px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 15L12 9L5 15" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
             </button>
